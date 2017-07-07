750eee161d8a88a423ae36bb889429b493db54eb
narimarc <narimarc>
2017-07-07 15:50:13 +0000

    git-svn-id: file:///home/mroda/Software/Genie/SVN/PhabGlobal/generator/devel/branches/inclpp@7106 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/HadronTransport/INCLparticletype.hh
+++ src/HadronTransport/INCLparticletype.hh
@@ -3,9 +3,11 @@
 
 #include "G4INCLCascade.hh"
 #include "GHEP/GHepParticle.h"
+#include "GHEP/GHepRecord.h"
 #include <list>
 #include <sstream>
 #include <TFile.h>
+#include <TLorentzVector.h>
 #include <TTree.h>
 using namespace genie;
 namespace G4INCL{
@@ -23,6 +25,15 @@ G4INCL::INCL *theINCLModel;
 		if(theConfig->getProjectileSpecies().theA==1&&theConfig->getProjectileSpecies().theZ==1) return pdg_codeProbe = 2212;  
 		else if(theConfig->getProjectileSpecies().theA ==1 && theConfig->getProjectileSpecies().theZ==0) return pdg_codeProbe = 2112;
 	}
+	int INCLtopdgcode(int A, int Z){
+  int pdg_codeP(0);
+  if (A==1 && Z==1)       return pdg_codeP = 2212;
+  else if(A==1 && Z==0)   return pdg_codeP = 2112;
+  else if ( A==0 && Z==0) return pdg_codeP=111;
+  else if (A==0 && Z==1)  return pdg_codeP = 211;
+  else if (A==0 && Z==-1) return pdg_codeP= -211;
+  else return pdg_codeP = genie::pdg::IonPdgCode( A , Z );
+}
 	G4INCL::ParticleType toINCLparticletype(int pdgc){
 
 		if       (pdgc == 2212)         return     G4INCL::Proton;
@@ -36,6 +47,24 @@ G4INCL::INCL *theINCLModel;
 		else if(pdgc == 1000020040)      return  G4INCL::Composite;
 		else                             return  G4INCL::UnknownParticle;
 	}
+GHepParticle *INCLtoGenieParticle(G4INCL::EventInfo result, int nP, GHepStatus_t    ist, int mom1, int mom2) {
+	int pdg_codeP(0);
+	double E_pnP(0), EKinP = result.EKin[nP];
+	TVector3 p3M(result.px[nP]/1000,result.py[nP]/1000,result.pz[nP]/1000);
+	TLorentzVector x4null(0.,0.,0.,0.);
+double 	m_pnP = 0.5*((result.px[nP])*(result.px[nP]) + (result.py[nP])*(result.py[nP]) + (result.pz[nP])*(result.pz[nP]) + EKinP*EKinP)/(EKinP);
+if (m_pnP<10&&result.A[nP]==0&&result.Z[nP]==0)
+          {
+            pdg_codeP = 22;
+            E_pnP = TMath::Sqrt((result.px[nP])*(result.px[nP])+(result.py[nP])*(result.py[nP])+(result.pz[nP])*(result.pz[nP])) ;  
+          }else{
+          	pdg_codeP  = INCLtopdgcode(result.A[nP],result.Z[nP]);
+            double Mass_prodPar = PDGLibrary::Instance()->Find(pdg_codeP)->Mass();
+            E_pnP = EKinP + Mass_prodPar*1000;
+          }
+          TLorentzVector p4tgtf(p3M,E_pnP/1000);
+          return new GHepParticle(pdg_codeP,ist,mom1,mom2,-1,-1,p4tgtf,x4null); 
+}   
   /*G4INCL::Particle *GenieparticletoINCLparticle(GHepParticle * p, G4INCL::ParticleType type) {
     TLorentzVector *v4= p->GetX4();;
     ThreeVector thePosition(0,0,0.);
