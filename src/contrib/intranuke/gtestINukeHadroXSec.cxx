bcf2728d1c2b60f0b0137c7718b4b6c95103c1f4
narimarc <narimarc>
2017-11-23 15:41:03 +0000

    git-svn-id: file:///home/mroda/Software/Genie/SVN/PhabGlobal/generator/devel/branches/inclpp@7179 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/contrib/intranuke/gtestINukeHadroXSec.cxx
+++ src/contrib/intranuke/gtestINukeHadroXSec.cxx
@@ -50,7 +50,7 @@
 using std::endl;
 using std::setw;
 using std::setfill;
-
+using namespace std;
 using namespace genie;
 
 void GetCommandLineArgs (int argc, char ** argv);
@@ -80,7 +80,7 @@ int main(int argc, char ** argv)
   string        fatestr   [nfates] = "  ";
   INukeFateHA_t fatetype  [nfates];
 
-  fatetype[0] = kIHAFtUndefined;   
+  fatetype[0] = kIHAFtUndefined;
   fatetype[1] = kIHAFtNoInteraction;
   fatetype[2] = kIHAFtCEx;
   fatetype[3] = kIHAFtElas;
@@ -89,10 +89,10 @@ int main(int argc, char ** argv)
   fatetype[6] = kIHAFtKo;
   fatetype[7] = kIHAFtPiProd;
   fatetype[8] = kIHAFtDCEx;
- 
+
   for (int k=0; k<nfates; k++) {
-    countfate[k] = 0; 
-    sigma    [k] = 0.; 
+    countfate[k] = 0;
+    sigma    [k] = 0.;
     sigma_err[k] = 0.;
     fatestr  [k] = INukeHadroFates::AsString(fatetype[k]);
   }
@@ -119,7 +119,7 @@ int main(int argc, char ** argv)
   thdr   = dynamic_cast <NtpMCTreeHeader *> ( file.Get("header") );
 
   /*if(!tree) {
-    LOG("gtestINukeHadroXSec", pERROR) 
+    LOG("gtestINukeHadroXSec", pERROR)
          << "No event tree found in input file!";
     return 1;
     }*/
@@ -131,7 +131,7 @@ int main(int argc, char ** argv)
 
     //    int nev = (int) tree->GetEntries();
     nev = (int) tree->GetEntries();
-    LOG("gtestINukeHadroXSec", pNOTICE) 
+    LOG("gtestINukeHadroXSec", pNOTICE)
       << "Processing " << nev << " events";
 
     //
@@ -148,16 +148,16 @@ int main(int argc, char ** argv)
 
       // extract info for the event sample
       if(ievent==0) {
-	kin_energy = event.Particle(0)->KinE();
-	probe_pdg  = event.Particle(0)->Pdg();
-	target_pdg = event.Particle(1)->Pdg();
+  kin_energy = event.Particle(0)->KinE();
+  probe_pdg  = event.Particle(0)->Pdg();
+  target_pdg = event.Particle(1)->Pdg();
       }
 
       // analyze
       const GHepRecord * grec = dynamic_cast<const GHepRecord *> (&event);
       INukeFateHA_t fate = FindhAFate(grec);
       if(ievent<displayno) {
-	LOG("gtestINukeHadroXSec", pNOTICE) 
+  LOG("gtestINukeHadroXSec", pNOTICE)
           << "fate = " << INukeHadroFates::AsString(fate);
       }
 
@@ -172,28 +172,28 @@ int main(int argc, char ** argv)
       case 5:   countfate[5]++; break;
       case 6:   countfate[6]++; break;
       case 13:  countfate[8]++; break;
-      default:  
-	if (7<=fate && fate<=12) countfate[7]++;
-	else {
-	  LOG("gtestINukeHadroXSec", pWARN) 
+      default:
+  if (7<=fate && fate<=12) countfate[7]++;
+  else {
+    LOG("gtestINukeHadroXSec", pWARN)
              << "Undefined fate from FindhAFate() : " << fate;
         }
-	break;
+  break;
       }
 
       // clear current mc event record
       mcrec->Clear();
 
-    } // end event loop 
+    } // end event loop
   } // end if (tree)
   else if ( ginuke ) {
     // possibly a ginuke file
 
-    LOG("gtestINukeHadroXSec", pNOTICE) 
+    LOG("gtestINukeHadroXSec", pNOTICE)
       << "Found ginuke type file";
 
     nev = (int) ginuke->GetEntries();
-    LOG("gtestINukeHadroXSec", pNOTICE) 
+    LOG("gtestINukeHadroXSec", pNOTICE)
       << "Processing " << nev << " events";
 
     int     kmax    = 250;
@@ -225,53 +225,53 @@ int main(int argc, char ** argv)
 
       // Determine fates (as defined in Intranuke/INukeUtils.cxx/ utils::intranuke::FindhAFate())
            if (energy==E_had[0] && numh==1) // No interaction
-	{ index=1; }
+  { index=1; }
       else if (energy!=E_had[0] && numh==1) // Elastic
-	{ index=3; }
+  { index=3; }
       else if ( pdg::IsPion(probe_pdg) && numpip+numpi0+numpim==0) // Absorption
-	{ index=5; }
+  { index=5; }
       else if ( (pdg::IsNucleon(probe_pdg) && numpip+numpi0+numpim==0 && numh>2 )
-		|| (probe_pdg==kPdgGamma && energy!=E_had[0] && numpip+numpi0+numpim==0)) // Knock-out
-	{ index=6; }
+    || (probe_pdg==kPdgGamma && energy!=E_had[0] && numpip+numpi0+numpim==0)) // Knock-out
+  { index=6; }
       else if ( numpip+numpi0+numpim> (pdg::IsPion(probe_pdg) ? 1 : 0) ) // Pion production
-	{ index=7; }
+  { index=7; }
       else if ( numh==2 ) // Inelastic or Charge Exchange
-	{
-	  if ( (pdg::IsPion(probe_pdg) && ( probe_pdg==pdg_had[0] || probe_pdg==pdg_had[1] ))
-	       || pdg::IsNucleon(probe_pdg) ) index=4;
-	  else index=2;
-	}
+  {
+    if ( (pdg::IsPion(probe_pdg) && ( probe_pdg==pdg_had[0] || probe_pdg==pdg_had[1] ))
+         || pdg::IsNucleon(probe_pdg) ) index=4;
+    else index=2;
+  }
       else //Double Charge Exchange or Undefined
-	{
-	  bool undef = true;
-	  if ( pdg::IsPion(probe_pdg) )
-	    {
-	      for (int iter = 0; iter < numh; iter++)
-		{
-		  if      (probe_pdg==211 && pdg_had[iter]==-211) { index=8; undef=false; }
-		  else if (probe_pdg==-211 && pdg_had[iter]==211) { index=8; undef=false; }
-		}
-	    }
-	  if (undef) { index=0; }
-	}
+  {
+    bool undef = true;
+    if ( pdg::IsPion(probe_pdg) )
+      {
+        for (int iter = 0; iter < numh; iter++)
+    {
+      if      (probe_pdg==211 && pdg_had[iter]==-211) { index=8; undef=false; }
+      else if (probe_pdg==-211 && pdg_had[iter]==211) { index=8; undef=false; }
+    }
+      }
+    if (undef) { index=0; }
+  }
       countfate[index]++;
       if (ievent<displayno) {
-	LOG("gtestINukeHadroXSec", pNOTICE) 
+  LOG("gtestINukeHadroXSec", pNOTICE)
           << "fate = " << INukeHadroFates::AsString(fatetype[index]);
-      } 
+      }
 
     }
   } // end if (ginuke)
   else {
-    LOG("gtestINukeHadroXSec", pERROR) 
+    LOG("gtestINukeHadroXSec", pERROR)
          << "Could not read input file!";
     return 1;
   }
-  
+
   //
   // output section
   //
- 
+
   const double fm2tomb  = units::fm2 / units::mb;
   const double dnev     = (double) nev;
   const int    NR       = 3;
@@ -286,13 +286,13 @@ int main(int argc, char ** argv)
   string probe_name  = pdglib->Find(probe_pdg)->GetName();
   string target_name = pdglib->Find(target_pdg)->GetName();
 
-  LOG("gtestINukeHadroXSec", pNOTICE) 
-     << " Probe = " << probe_name 
+  LOG("gtestINukeHadroXSec", pNOTICE)
+     << " Probe = " << probe_name
      << ", KE = " << kin_energy << " GeV";
-  LOG("gtestINukeHadroXSec", pNOTICE) 
-    << " Target = " << target_name 
-    << " (Z,A) = (" << Z << ", " << A 
-    << "), nuclear radius = " << nuclear_radius 
+  LOG("gtestINukeHadroXSec", pNOTICE)
+    << " Target = " << target_name
+    << " (Z,A) = (" << Z << ", " << A
+    << "), nuclear radius = " << nuclear_radius
     << " fm, area = " << area << " fm**2 " << '\n';
 
   int    cnttot     = 0;
@@ -312,16 +312,16 @@ int main(int argc, char ** argv)
          sigma_err[k] = fm2tomb * area * TMath::Sqrt(countfate[k])/dnev;
       }
       if(countfate[k]>0) {
-        LOG("gtestINukeHadroXSec", pNOTICE) 
-            << " --> " << setw(26) << fatestr[k] 
-            << ": " << setw(7) << countfate[k] << " events -> " 
+        LOG("gtestINukeHadroXSec", pNOTICE)
+            << " --> " << setw(26) << fatestr[k]
+            << ": " << setw(7) << countfate[k] << " events -> "
             << setw(7) << sigma[k] << " +- " << sigma_err[k] << " (mb)" << '\n';
       }
       if(k==5) {
         sigtotAbs += sigma[k];
       }
-      else 
-      if (k!=1) { 
+      else
+      if (k!=1) {
         sigtotScat += sigma[k];
       }
     }//k!=1
@@ -334,42 +334,42 @@ int main(int argc, char ** argv)
   double sigtoterr_noelas = fm2tomb * area * TMath::Sqrt(cnttot-countfate[3])/dnev;
 
   double ratio_as = (sigtotScat==0) ? 0 : sigtotAbs/(double)sigtotScat;
-  
-  LOG("gtestINukeHadroXSec", pNOTICE) 
-    << "\n\n --------------------------------------------------- " 
-    << "\n ==> " << setw(28) << " Total: " << setw(7) << cnttot 
+
+  LOG("gtestINukeHadroXSec", pNOTICE)
+    << "\n\n --------------------------------------------------- "
+    << "\n ==> " << setw(28) << " Total: " << setw(7) << cnttot
     << " events -> " << setw(7) << sigtot << " +- " << sigtoterr << " (mb)"
-    << "\n (-> " << setw(28) << " Hadrons escaped nucleus: " 
+    << "\n (-> " << setw(28) << " Hadrons escaped nucleus: "
     << setw(7) << nullint << " events)"
-    << "\n ==> " << setw(28) << " Ratio (abs/scat) = " 
+    << "\n ==> " << setw(28) << " Ratio (abs/scat) = "
     << setw(7) << ratio_as
-    << "\n ==> " << setw(28) << " avg. num of int. = " 
+    << "\n ==> " << setw(28) << " avg. num of int. = "
     << setw(7) << cnttot/dnev
-    << "\n ==> " << setw(28) << " no interaction   = " 
+    << "\n ==> " << setw(28) << " no interaction   = "
     << setw(7) << (dnev-cnttot)/dnev
     << "\n ------------------------------------------------------- \n";
 
-  if(gOptWriteOutput) 
+  if(gOptWriteOutput)
   {
     ifstream test_file;
     bool file_exists=false;
     test_file.open(gOptOutputFilename.c_str(), std::ifstream::in);
     file_exists=test_file.is_open();
     test_file.close();
-    ofstream xsec_file; 
+    ofstream xsec_file;
     xsec_file.open(gOptOutputFilename.c_str(), std::ios::app);
     if (!file_exists)
       {
-	xsec_file << "#KE" << "\t" << "Undef" << "\t"
-		  << "sig" << "\t" << "CEx"   << "\t"
-		  << "sig" << "\t" << "Elas"  << "\t"
-		  << "sig" << "\t" << "Inelas"<< "\t"
-		  << "sig" << "\t" << "Abs"   << "\t"
-		  << "sig" << "\t" << "KO"    << "\t"
-		  << "sig" << "\t" << "PiPro" << "\t"
-		  << "sig" << "\t" << "DCEx"  << "\t"
-		  << "sig" << "\t" << "Reac"  << "\t"
-		  << "sig" << "\t" << "Tot"   << "\t" << "sig" << endl;
+  xsec_file << "#KE" << "\t" << "Undef" << "\t"
+      << "sig" << "\t" << "CEx"   << "\t"
+      << "sig" << "\t" << "Elas"  << "\t"
+      << "sig" << "\t" << "Inelas"<< "\t"
+      << "sig" << "\t" << "Abs"   << "\t"
+      << "sig" << "\t" << "KO"    << "\t"
+      << "sig" << "\t" << "PiPro" << "\t"
+      << "sig" << "\t" << "DCEx"  << "\t"
+      << "sig" << "\t" << "Reac"  << "\t"
+      << "sig" << "\t" << "Tot"   << "\t" << "sig" << endl;
       }
     xsec_file << kin_energy;
     for(int k=0; k<nfates; k++) {
@@ -394,15 +394,20 @@ INukeFateHA_t FindhAFate(const GHepRecord * evrec)
   double p_pdg = evrec->Probe()->Pdg();
 
   // particle codes
-  int numtype[] = {kPdgProton, kPdgNeutron, kPdgPiP, kPdgPiM, kPdgPi0, kPdgKP, kPdgKM, kPdgK0, kPdgGamma};
+  const int kPdgDeuteron = 1000010020; // pdg::IonPdgCode(2,1);
+  const int kPdgTritium  = 1000010030; // pdg::IonPdgCode(3,1);
+  const int kPdgAlpha    = 1000020040; // pdg::IonPdgCode(4,2);
+  const int kPdgHe3      = 1000020030;
+  //int numtype[] = {kPdgProton, kPdgNeutron, kPdgPiP, kPdgPiM, kPdgPi0, kPdgKP, kPdgKM, kPdgK0, kPdgGamma};
+  int numtype[] = {kPdgProton, kPdgNeutron, kPdgPiP, kPdgPiM, kPdgPi0, kPdgKP, kPdgKM, kPdgK0, kPdgGamma,kPdgDeuteron,kPdgTritium,kPdgAlpha,kPdgHe3};
   // num of particle for numtype
-  int num[]  = {0,0,0,0,0,0,0,0,0};
+  int num[]  = {0,0,0,0,0,0,0,0,0,0,0,0,0};
   int num_t  = 0;
   int num_nu = 0;
   int num_pi = 0;
   int num_k  = 0;
   // max KE for numtype
-  double numKE[] = {0,0,0,0,0,0,0,0,0};
+  double numKE[] = {0,0,0,0,0,0,0,0,0,0,0,0,0};
 
   GHepStatus_t status = kIStUndefined;
 
@@ -419,7 +424,7 @@ INukeFateHA_t FindhAFate(const GHepRecord * evrec)
     status=p->Status();
     if(status==kIStStableFinalState)
     {
-      switch((int) p->Pdg()) 
+      switch((int) p->Pdg())
       {
         case ((int) kPdgProton)  : index = 0; break;
         case ((int) kPdgNeutron) : index = 1; break;
@@ -430,11 +435,15 @@ INukeFateHA_t FindhAFate(const GHepRecord * evrec)
         case ((int) kPdgKM)      : index = 6; break;
         case ((int) kPdgK0)      : index = 7; break;
         case ((int) kPdgGamma)   : index = 8; break;
-        case (2000000002)        : index = 9; hasBlob=true; break;
-                          default: index = 9; break;
+        case ((int)kPdgDeuteron) : index = 9; break;
+        case ((int)kPdgTritium)  : index =10; break;
+        case ((int)kPdgAlpha)    : index =11; break;
+        case ((int)kPdgHe3)      : index =12; break;
+        case (2000000002)        : index = 13; hasBlob=true; break;
+                          default: index = 13; break;
       }
 
-      if(index!=9)
+      if(index!=13)
       {
         if(numFsPart==0) fs=p;
         numFsPart++;
@@ -451,7 +460,7 @@ INukeFateHA_t FindhAFate(const GHepRecord * evrec)
     double dPy = TMath::Abs( probe->Py() - fs->Py() );
     double dPx = TMath::Abs( probe->Px() - fs->Px() );
 
-    if (dE < 1e-15 && dPz < 1e-15 && dPy < 1e-15 && dPx < 1e-15) return kIHAFtNoInteraction;
+    if (dE < 1e-15 && dPz < 1e-15 && dPy < 1e-15 && dPx < 1e-15) return kIHAFtNoInteraction ; 
   }
 
   num_t  = num[0]+num[1]+num[2]+num[3]+num[4]+num[5]+num[6]+num[7];
@@ -482,7 +491,7 @@ INukeFateHA_t FindhAFate(const GHepRecord * evrec)
   else if(num_k<((p_pdg==kPdgKP || p_pdg==kPdgKM || p_pdg==kPdgK0)?(1):(0)))
   {
     return kIHAFtAbs;
-  }  
+  }
   else
   {
     if(p_pdg==kPdgPiP || p_pdg==kPdgPiM || p_pdg==kPdgPi0
@@ -495,11 +504,11 @@ INukeFateHA_t FindhAFate(const GHepRecord * evrec)
       else if(num[5]==1) { fs_pdg=kPdgKP; fs_ind=5; }
       else if(num[6]==1) { fs_pdg=kPdgKM; fs_ind=6; }
       else               { fs_pdg=kPdgK0; fs_ind=7; }
- 
+
       if(p_pdg==fs_pdg)
       {
-	if(num_nu==0) return kIHAFtElas;
-	else return kIHAFtInelas;
+  if(num_nu==0) return kIHAFtElas;
+  else return kIHAFtInelas;
       }
       else if(((p_pdg==kPdgPiP || p_pdg==kPdgPiM) && fs_ind==4) ||
               ((fs_ind==2 || fs_ind==3) && p_pdg==kPdgPi0))
@@ -527,40 +536,47 @@ INukeFateHA_t FindhAFate(const GHepRecord * evrec)
       int fs_ind;
       if(num[0]>=1) { fs_ind=0; }
       else          { fs_ind=1; }
-
-      if(num_nu==1)
+  if(num_nu==0){
+  if (numtype[9]==kPdgDeuteron||numtype[10]==kPdgTritium||numtype[11]==kPdgAlpha||numtype[12]==kPdgHe3) {
+         return kIHAFtKo;
+        }
+  }
+      else if(num_nu==1)
       {
-        if(numtype[fs_ind]==p_pdg) return kIHAFtElas;
-        else return kIHAFtUndefined;
+        if(numtype[fs_ind]==p_pdg) {return kIHAFtElas;}
+        else if (numtype[9]==kPdgDeuteron||numtype[10]==kPdgTritium||numtype[11]==kPdgAlpha||numtype[12]==kPdgHe3) {
+         return kIHAFtKo;
+       }else {return kIHAFtUndefined;}
+      
       }
       else if(num_nu==2)
       {
-        if(numKE[1]>numKE[0]) { fs_ind=1; }  
-        
+        if(numKE[1]>numKE[0]) { fs_ind=1; }
+
         if(numtype[fs_ind]==p_pdg)
-	  {
+    {
           //if(numKE[fs_ind]>=(.8*p_KE))
           //{
           //  if(num[0]==1 && num[1]==1) return kIHAFtKo;
           //  else if(num[0]==2) return kIHAFtKo;
-	  //  else return kIHAFtKo;
+    //  else return kIHAFtKo;
           //}
           //else
-	     return kIHAFtInelas; //fix later
+       return kIHAFtInelas; //fix later
         }
         else
         {
-	  // if(numKE[fs_ind]>=(.8*p_KE)) return kIHAFtInelas;
-	  // else
-	  // {
+    // if(numKE[fs_ind]>=(.8*p_KE)) return kIHAFtInelas;
+    // else
+    // {
           //  if(num[fs_ind]==2)
           //  {
           //    if(num[0]==2) return kIHAFtKo;
           //    else return kIHAFtKo;
           //  }
           //  else return kIHAFtInelas;
-	  // }
-	  return kIHAFtInelas; //fix later
+    // }
+    return kIHAFtInelas; //fix later
         }
       }
       else if(num_nu>2)
@@ -583,7 +599,6 @@ INukeFateHA_t FindhAFate(const GHepRecord * evrec)
       if(numKE[fs_ind]>=(.8*p_KE)) return kIHAFtElas;
       else return kIHAFtInelas;
     }
-    else if (p_pdg==kPdgGamma)
     {
       if     (num[0]==2 && num[1]==1) return kIHAFtKo;
       else if(num[0]==1 && num[1]==2) return kIHAFtKo;
@@ -627,7 +642,7 @@ void GetCommandLineArgs(int argc, char ** argv)
 void PrintSyntax(void)
 {
   LOG("gtestINukeHadroXSec", pNOTICE)
-    << "\n\n" 
+    << "\n\n"
     << "Syntax:" << "\n"
     << "  gtestINukeHadroXSec -f event_file [-w]"
     << "\n";
